26d69eeb3cb2f160c555a294e79b9fac3f593218
Charlene Castro <charlene.castro@torontomu.ca>
2023-11-18 17:59:22 -0500

    made repeatString return str repeated num times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeated = "";
+    for(let i=0; i<num; i++){
+        repeated += str;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
