3284d158f8d92c310ca81fb1ef5bad29a3889976
Charlene Castro <charlene.castro@torontomu.ca>
2023-11-19 02:12:39 -0500

    added alt soln using summation formula

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,3 +1,4 @@
+/*
 const sumAll = function(a, b) {
     if(a<0 || b<0 || typeof a !== 'number' || typeof b !== 'number'){
         return "ERROR"
@@ -11,6 +12,25 @@ const sumAll = function(a, b) {
     for(let i=min; i<=max; i++){
         sum+=i;
     }
+    return sum;
+};
+*/
+
+const sumAll = function(a, b) {
+    if(a<0 || b<0 || typeof a !== 'number' || typeof b !== 'number'){
+        return "ERROR"
+    }
+
+    //summation formula
+    /*
+    var n=a+b-1;
+    var sum = n*(n+1)/2;
+    */
+
+    //simplified version:
+    var n=a+b;
+    var sum = (n-1)*(n)/2;
+
     return sum;
 };
 
