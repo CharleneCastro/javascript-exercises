054d74fab12aa63f10e8f2d94b941ffbac33904e
Charlene Castro <charlene.castro@torontomu.ca>
2023-11-19 01:47:35 -0500

    made sumAll() calc the summation of nums a to b

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    var min=a, max=b, sum=0;
+    if(a>b){
+        min=b;
+        max=a;
+    }
+    for(let i=min; i<=max; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
