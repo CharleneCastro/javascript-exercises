0d93158b85c54927756cf44ce72dc00488a2d337
Charlene Castro <charlene.castro@torontomu.ca>
2023-11-19 00:44:06 -0500

    added 2nd soln with filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
+/*
 const removeFromArray = function(arr, ...args) {
-    // var removed=[];
     for(let i=0; i<args.length; i++){
         for(let j=0; j<arr.length; j++){
             if(args[i] === arr[j]){
@@ -9,6 +9,14 @@ const removeFromArray = function(arr, ...args) {
     }
     return arr;
 };
+*/
+
+const removeFromArray = function(arr, ...args) {
+    //filter() goes through each index of arr (arrVal) &  keeps indexes where !args.includes(arrVal) is True
+    //!args.includes(arrVal) checks that arrVal is not a value in args
+    return arr.filter((arrVal) => !args.includes(arrVal));
+};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
