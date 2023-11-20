0a0157d1d8b0b4224d4b103f07e31c2ef23e8ae7
Charlene Castro <charlene.castro@torontomu.ca>
2023-11-19 22:15:46 -0500

    made convertToFahrenheit() return ans w 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,8 @@ const convertToCelsius = function(deg) {
   return Math.round(((deg-32)*5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  return Math.round((deg*9/5 +32)*10)/10;
 };
 
 // Do not edit below this line
